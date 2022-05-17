b99fe228290ebfeb42587498352024b624abe740
Kyle D'Arcy <kyle.darcy@icloud.com>
2022-05-17 00:49:46 -0400

    updating

--- lambda_function.py
+++ lambda_function.py
@@ -15,17 +15,16 @@ def lambda_handler(event, context):
     try:
         canyon_html = get_html(CANYON_URL)
         sizes_list = find_stock(canyon_html)
-        print(sizes_list)
         old_stock = get_old_stock()
         if old_stock != ("\n").join(sizes_list):
-            Twilio.send_text(MY_PHONE_NUMBER, "ALERT: Canyon has updated their stock" + "\n".join(sizes_list))
-            # Twilio.make_call(MY_PHONE_NUMBER)
+            # Twilio.send_text(MY_PHONE_NUMBER, "ALERT: Canyon has updated their stock" + "\n".join(sizes_list))
+            Twilio.make_call(MY_PHONE_NUMBER)
             table.put_item(Item={'bike-name': BIKE_KEY, 'stock': ("\n").join(sizes_list)})
         return {
             'statusCode': 200,
             'body': json.dumps(sizes_list)
         }
     except Exception as e:
-        Twilio.send_text("bike-stock-check failed with with: {}".format(e))
-        # Twilio.make_call(MY_PHONE_NUMBER)
+        # Twilio.send_text("bike-stock-check failed with with: {}".format(e))
+        Twilio.make_call(MY_PHONE_NUMBER)
         
